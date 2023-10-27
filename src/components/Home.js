e5e7670a6f682eac66c6dd6774346d2430fff852
Md-Shajib <mdshajib8190@gmail.com>
2023-10-28 03:51:10 +0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import Todos from './Todos'
+import {v4 as uuidv4} from 'uuid';
+
+import style from "./home.module.css"
+import NewTodo from './NewTodo'
+
+
+const Home = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (todo) => {
+  setTodos( (prevTodos) => {
+    return [...prevTodos, {id: uuidv4(), todo}]
+  })
+  };
+
+  const handleRemoveTodo = (id) => {
+    setTodos((prevTodos) => {
+      const filterTodos = prevTodos.filter( (todo) => todo.id !== id);
+      return filterTodos;
+    });
+  };
+
+  return (
+    <div className={style.container}>
+        <h1>Todo App</h1>
+        <NewTodo onAddTodo={handleAddTodo}/>
+        <Todos todos = {todos} onRemoveTodo = {handleRemoveTodo}/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/NewTodo.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+
+import style from './newtodo.module.css'
+
+const NewTodo = (props) => {
+
+  const [todo, setTodo] = useState({title: " ", desc: " "});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setTodo( (oldTodo) => {
+        return{...oldTodo, [name]: event.target.value}
+    })
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onAddTodo(todo);
+    setTodo({title: " ", desc: " "});
+  };
+  
+  return <form className={style.form} onSubmit = {handleSubmit}>
+    <div className={style["form-field"]}>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id='title' name='title' value={todo.title} onChange={handleChange}/>
+    </div>
+    <div className={style["form-field"]}>
+        <label htmlFor="desc">Description:</label>
+        <textarea type="text" id='desc' name='desc' value={todo.desc} onChange={handleChange}/>
+    </div>
+    <button type='submit'>Add Todo</button>
+  </form>
+}
+
+export default NewTodo
